334d4c42e7bf6d9b03ad8c457f75983e511eaafb
Serhii Kovalov <serg3@Serhiis-MacBook-Pro.local>
2021-05-08 18:56:46 +0200

    scan with flag

---
+++ cmd/srch/srch.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go_core/02_search_engine/pkg/crawler"
+	"go_core/02_search_engine/pkg/crawler/spider"
+	"log"
+	"strings"
+)
+
+func main() {
+	param := flag.String("s", "", "word for search")
+	flag.Parse()
+	if *param != "" {
+		docs := scan()
+		fmt.Println("Search result:")
+		for _, doc := range docs {
+			if strings.Contains(strings.ToLower(doc.Title), strings.ToLower(*param)) {
+				fmt.Println(doc.URL, doc.Title)
+			}
+		}
+	} else {
+		fmt.Println("Flag required!")
+	}
+}
+
+// Function scan() uses package 'crawler'
+// to search through Go sites by word
+func scan() (docs []crawler.Document) {
+	scn := spider.New()
+	const dep = 2
+	urls := []string{"https://golang.org", "https://go.dev"}
+	for _, url := range urls {
+		res, err := scn.Scan(url, dep)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		docs = append(docs, res...)
+	}
+	return docs
+}
